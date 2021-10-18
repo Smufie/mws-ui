ffc84841253be8e0fcbed0e321d576a1d561e88e
BlazejMumot <blazej.mumot99@gmail.com>
2021-10-18 19:15:12 +0200

    Added TemporaryDrawer

--- mws-ui/src/components/common/Navbar.js
+++ mws-ui/src/components/common/Navbar.js
@@ -1,32 +1,78 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
+import * as React from "react";
 import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import AppBar from "@mui/material/AppBar";
+import MenuIcon from "@mui/icons-material/Menu";
+import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
-function Navbar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{ background: "#2E3B55" }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Mobile Weather Station
-          </Typography>
-        </Toolbar>
-      </AppBar>
+export default function TemporaryDrawer() {
+  const [state, setState] = React.useState({
+    left: false,
+  });
+
+  const toggleDrawer = (open) => (event) => {
+    setState({ ...state, ["left"]: open });
+  };
+
+  const list = () => (
+    <Box>
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
     </Box>
   );
+
+  return (
+    <AppBar position="fixed" style={{ background: "#2E3B55" }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
+          <MenuIcon onClick={toggleDrawer(true)} />
+        </IconButton>
+        <Drawer
+          anchor={"left"}
+          open={state["left"]}
+          onClose={toggleDrawer(false)}
+        >
+          {list()}
+        </Drawer>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Mobile Weather Station
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
-export default Navbar;
